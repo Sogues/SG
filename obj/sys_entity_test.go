fe99623b11175c106ef6661e17ff6f4a920090e1
Sogues <mail.caoke@icloud.com>
2021-12-01 10:22:28 +0800

    update

--- obj/sys_entity_test.go
+++ obj/sys_entity_test.go
@@ -1,18 +1,52 @@
 package obj
 
-import "github.com/Sogues/ETForGo/types"
+import (
+	"testing"
 
-type testRoleEntity struct {
-	BaseEntity
-}
+	"github.com/Sogues/ETForGo/types"
+)
+
+type (
+	testRoleEntity struct {
+		BaseEntity
+	}
+	testRoleEntityAwakeParam struct {
+	}
+)
 
 func (t testRoleEntity) EntityTypeId() types.EntityType {
 	return types.EntityTypeTest1
 }
 
 type testRoleAwakeSystem struct {
+	*AwakeBaseSystem
 }
 
 func (testRoleAwakeSystem) ComponentTypeId() types.EntityType {
 	return testRoleEntity{}.EntityTypeId()
+}
+
+func (testRoleAwakeSystem) Awake(component, param interface{}) {
+	cp, ok := component.(*testRoleEntity)
+	if !ok {
+		return
+	}
+	p, ok := param.(testRoleEntityAwakeParam)
+	if !ok {
+		return
+	}
+	testRoleAwakeSystem{}.awake(cp, p)
+}
+
+func (testRoleAwakeSystem) awake(cmp *testRoleEntity, param testRoleEntityAwakeParam) {
+}
+
+func TestCombine(t *testing.T) {
+	s := &testRoleAwakeSystem{}
+	s.impl = s
+}
+
+var b1
+func BenchmarkTestRoleAwakeSystemAwake(b *testing.B) {
+
 }
