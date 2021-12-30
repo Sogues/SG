280c66d3005f19e25768b83260dee4829a82e226
Sogues <mail.caoke@icloud.com>
2021-12-31 01:34:02 +0800

    update

--- core/SGTime/sgTime.go
+++ core/SGTime/sgTime.go
@@ -1,12 +1,11 @@
 package SGTime
 
-import "time"
-
 const (
-	ticksPerMill int64 = 10
-	ticksPerSec  int64 = 1000 * ticksPerMill
-	tickPerMin   int64 = 60 * ticksPerSec
-	tickPerHour  int64 = 60 * tickPerMin
+	ticksPerMicro int64 = 10
+	ticksPerMill  int64 = 1000 * ticksPerMicro
+	ticksPerSec   int64 = 1000 * ticksPerMill
+	tickPerMin    int64 = 60 * ticksPerSec
+	tickPerHour   int64 = 60 * tickPerMin
 )
 
 var (
@@ -15,15 +14,18 @@ var (
 
 type (
 	spanTime struct {
+		// 每100us为1tick
 		ticks int64
 	}
 	SGTime struct {
 		accElpTime spanTime
 	}
 	SGTick struct {
-		nano int64
+		startNano int64
 
-		elp spanTime
+		elp   spanTime
+		start spanTime
+		total spanTime
 
 		paused bool
 	}
@@ -32,23 +34,35 @@ type (
 	}
 )
 
-func (s *spanTime) ToMill() int64 {
+func (s spanTime) ToNano() int64 {
+	return s.ticks * 10
+}
+
+func (s spanTime) ToMicro() int64 {
+	return s.ticks / ticksPerMicro
+}
+
+func (s spanTime) ToMill() int64 {
 	return s.ticks / ticksPerMill
 }
 
-func (s *spanTime) ToSec() int64 {
+func (s spanTime) ToSec() int64 {
 	return s.ticks / ticksPerSec
 }
 
-func (s *spanTime) Add(st *spanTime) {
-	s.ticks += st.ticks
+func (s spanTime) Add(st spanTime) spanTime {
+	return spanTime{s.ticks + st.ticks}
+}
+
+func genSpanTimeFromNano(ns int64) spanTime {
+	return spanTime{ns / 10}
 }
 
 func (t *SGTick) Tick() {
-	if t.paused {
-		t.elp = spTimeZero
-		return
-	}
-	rawNano := time.Now().UnixNano()
-	_ = rawNano
+	//if t.paused {
+	//	t.elp = spTimeZero
+	//	return
+	//}
+	//rawNano := time.Now().UnixNano()
+	//t.total = t.start.Add(genSpanTimeFromNano(rawNano - t.startNano))
 }
