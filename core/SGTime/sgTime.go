e8d2a398456fb88087c198f39dd3a11a84cdc7c7
ck <ck>
2021-12-31 18:13:59 +0800

    update

--- core/SGTime/sgTime.go
+++ core/SGTime/sgTime.go
@@ -1,5 +1,7 @@
 package SGTime
 
+import "time"
+
 const (
 	ticksPerMicro int64 = 10
 	ticksPerMill  int64 = 1000 * ticksPerMicro
@@ -19,21 +21,44 @@ type (
 	}
 	SGTime struct {
 		accElpTime spanTime
+
+		totalTime spanTime
+		eplTime   spanTime
+
+		isRunningSlowly bool
 	}
 	SGTick struct {
 		startNano int64
+		lastNano  int64
 
-		elp   spanTime
-		start spanTime
-		total spanTime
+		elp                spanTime
+		elpWithPause       spanTime
+		start              spanTime
+		totalTime          spanTime
+		totalTimeWithPause spanTime
 
-		paused bool
+		paused         bool
+		pauseStartNano int64
+		pausedTime     int64
 	}
 	FixedUpdate struct {
 		updateTime SGTime
+
+		playTick   *SGTick
+		updateTick *SGTick
+		tick       *SGTick
 	}
 )
 
+func (f *FixedUpdate) Tick() {
+	f.tick.Tick()
+	f.playTick.Tick()
+	f.updateTick.Reset()
+}
+
+func genSpanTimeFromNano(ns int64) spanTime {
+	return spanTime{ns / 10}
+}
 func (s spanTime) ToNano() int64 {
 	return s.ticks * 10
 }
@@ -54,15 +79,48 @@ func (s spanTime) Add(st spanTime) spanTime {
 	return spanTime{s.ticks + st.ticks}
 }
 
-func genSpanTimeFromNano(ns int64) spanTime {
-	return spanTime{ns / 10}
+func (s spanTime) LTZero() bool {
+	return s.ticks < 0
+}
+
+func (t *SGTick) Reset() {
+	t.start = spTimeZero
+	t.totalTime = spTimeZero
+	t.startNano = time.Now().UnixNano()
+	t.lastNano = t.startNano
+	t.pausedTime = 0
+	t.paused = false
 }
 
 func (t *SGTick) Tick() {
-	//if t.paused {
-	//	t.elp = spTimeZero
-	//	return
-	//}
-	//rawNano := time.Now().UnixNano()
-	//t.total = t.start.Add(genSpanTimeFromNano(rawNano - t.startNano))
+	if t.paused {
+		t.elp = spTimeZero
+		return
+	}
+	rawNano := time.Now().UnixNano()
+	t.totalTime = t.start.Add(genSpanTimeFromNano(rawNano - t.startNano - t.pausedTime))
+	t.totalTimeWithPause = t.start.Add(genSpanTimeFromNano(rawNano - t.startNano))
+	t.elp = genSpanTimeFromNano(rawNano - t.pausedTime - t.lastNano)
+	t.elpWithPause = genSpanTimeFromNano(rawNano - t.lastNano)
+	if t.elp.LTZero() {
+		t.elp = spTimeZero
+	}
+	t.lastNano = rawNano
+}
+
+func (t *SGTick) Pause() {
+	if t.paused {
+		return
+	}
+	t.paused = true
+	t.pauseStartNano = time.Now().UnixNano()
+}
+
+func (t *SGTick) Resume() {
+	if !t.paused {
+		return
+	}
+	t.paused = false
+	t.pausedTime += time.Now().UnixNano() - t.pauseStartNano
+	t.pauseStartNano = 0
 }
